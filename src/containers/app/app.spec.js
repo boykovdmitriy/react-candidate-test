5ac40e65567a0dabccf5a156df329e534501db2c
Dmitriy Boykov <boikovdmitriy@mail.ru>
2019-04-17 01:13:13 +0300

    [ADD] added test environment

---
+++ src/containers/app/app.spec.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {App} from './index'
+
+test('render App component', () => {
+  const wrapper = shallow(
+    <App/>
+  );
+  expect(wrapper).toMatchSnapshot();
+});
